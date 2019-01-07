050fecbc68c709dc774b0b530ada148ebac8db07
Stephanie Hsu <hsusteph5@gmail.com>
2019-01-07 12:20:51 -0800

    Add webpack modules and entry file for the frontend

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[hash]-[name].[ext]',
+            publicPath: 'images/',
+            outputPath: '../../../public/images/'
+          }
+        }]
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
