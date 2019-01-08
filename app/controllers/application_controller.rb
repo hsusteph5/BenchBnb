52a2d209c44a537058b2fd6f0901a37870acf599
Stephanie Hsu <hsusteph5@gmail.com>
2019-01-07 18:18:36 -0800

    Add user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+    def login(user)
+        session[:session_token] = user 
+        @current_user = user 
+    end 
+
+    def logout
+        session[:session_token] = nil 
+        current_user.reset_session_token!
+    end 
+
+    def require_login
+        redirect_to new_session_url unless logged_in?
+    end 
+
+    def logged_in?
+        !!@current_user
+    end 
+
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end 
+    
+
 end
