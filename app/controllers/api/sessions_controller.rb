b019eac51aba1b55ca20cee6bf0c5acc73b9e2f1
Stephanie Hsu <hsusteph5@gmail.com>
2019-01-07 21:19:51 -0800

    Add views for users and session

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,20 @@
 class Api::SessionsController < ApplicationController
+    def create 
+        @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+        if @user 
+            render "api/users/show"
+        else   
+            render json: ['The email or password you entered is incorrect'], status: 401
+        end
+    end 
+
+
+    def destroy 
+        if current_user 
+            current_user.reset_session_token!
+            render json: {}
+        else
+            render json: {}, status: 404
+        end
+    end 
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,3 +1,15 @@
 class Api::UsersController < ApplicationController
-    
+    def create 
+        @user = User.new(user_params)
+        if @user.save 
+            login(@user)
+            # redirect_to 
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end 
+
+    def user_params
+        params.require(:user).permit(:fname, :lname, :email, :password)
+    end 
 end
